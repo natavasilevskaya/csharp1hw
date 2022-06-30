231abf5c1270a3943be953a6302afaa69a16fc4c
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-01 00:24:43 +0300

    Task3 file added

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int number1;
+
+Console.WriteLine("Imput number: ");
+number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1%2 == 0)
+{
+    Console.Write(number1 + " is an even number!");
+}
+else
+{
+    Console.Write(number1 + " is an odd number!");
+}
