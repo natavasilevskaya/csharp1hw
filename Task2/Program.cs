0d701ec4c39b7f0bfc10aafcea70ea0adb3e4121
Nata.Vasilevskaya <_nata.vasilevskaya11@yandex.ru>
2022-07-01 00:16:50 +0300

    Task2 file added

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Imput number1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput number2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Imput number3: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n2>max) max = n2;
+if (n3>max) max = n3;
+Console.Write("Max number is " + max );
